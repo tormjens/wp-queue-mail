dcc988bc3c3e74c492ea325199e2c93317060779
Tor Morten Jensen <tormorten@tormorten.no>
2017-08-03 10:47:23 +0200

    remove mc_debug

--- includes/vendor/queue/classes/worker/wp-worker.php
+++ includes/vendor/queue/classes/worker/wp-worker.php
@@ -1,96 +1,95 @@
 <?php
 
-if ( ! class_exists( 'WP_Worker' ) ) {
-	class WP_Worker {
+if (! class_exists('WP_Worker')) {
+    class WP_Worker
+    {
 
-		/**
-		 * @var WP_Queue
-		 */
-		protected $queue;
+        /**
+         * @var WP_Queue
+         */
+        protected $queue;
 
-		/**
-		 * @var WP_Job
-		 */
-		protected $payload;
+        /**
+         * @var WP_Job
+         */
+        protected $payload;
 
-		/**
-		 * WP_Worker constructor.
-		 *
-		 * @param WP_Queue $queue
-		 */
-		public function __construct( $queue ) {
-			$this->queue = $queue;
-		}
+        /**
+         * WP_Worker constructor.
+         *
+         * @param WP_Queue $queue
+         */
+        public function __construct($queue)
+        {
+            $this->queue = $queue;
+        }
 
-		/**
-		 * Should run
-		 *
-		 * @return bool
-		 */
-		public function should_run() {
-			if ( $this->queue->available_jobs() ) {
-				return true;
-			}
-
-			return false;
-		}
-
-		/**
-		 * Process next job.
-		 *
-		 * @return bool
-		 */
-		public function process_next_job() {
-			$job = $this->queue->get_next_job();
-
-			if (empty($job)) {
-			    mailchimp_debug('wp-worker@process_next_job', 'empty job data');
-			    return true;
+        /**
+         * Should run
+         *
+         * @return bool
+         */
+        public function should_run()
+        {
+            if ($this->queue->available_jobs()) {
+                return true;
             }
 
-			$this->payload = unserialize( $job->job );
+            return false;
+        }
 
-			$this->queue->lock_job( $job );
-			$this->payload->set_job( $job );
+        /**
+         * Process next job.
+         *
+         * @return bool
+         */
+        public function process_next_job()
+        {
+            $job = $this->queue->get_next_job();
 
-			try {
-				$this->payload->handle();
+            $this->payload = unserialize($job->job);
 
-				if ( $this->payload->is_released() ) {
-					// Job manually released, release back onto queue
-					$this->queue->release( $job, $this->payload->get_delay() );
-				}
+            $this->queue->lock_job($job);
+            $this->payload->set_job($job);
 
-				if ( $this->payload->is_deleted() ) {
-					// Job manually deleted, delete from queue
-					$this->queue->delete( $job );
-				}
+            try {
+                $this->payload->handle();
 
-				if ( ! $this->payload->is_deleted_or_released() ) {
-					// Job completed, delete from queue
-					$this->queue->delete( $job );
-				}
-			} catch ( Exception $e ) {
-				$this->queue->release( $job );
+                if ($this->payload->is_released()) {
+                    // Job manually released, release back onto queue
+                    $this->queue->release($job, $this->payload->get_delay());
+                }
 
-				return false;
-			}
+                if ($this->payload->is_deleted()) {
+                    // Job manually deleted, delete from queue
+                    $this->queue->delete($job);
+                }
 
-			if (defined('WP_CLI') && WP_CLI && property_exists($this->payload, 'should_kill_queue_listener') && $this->payload->should_kill_queue_listener === true) {
-			    wp_die('killing queue listener');
+                if (! $this->payload->is_deleted_or_released()) {
+                    // Job completed, delete from queue
+                    $this->queue->delete($job);
+                }
+            } catch (Exception $e) {
+                $this->queue->release($job);
+
+                return false;
             }
 
-			return true;
-		}
+            if (defined('WP_CLI') && WP_CLI && property_exists($this->payload, 'should_kill_queue_listener') && $this->payload->should_kill_queue_listener === true) {
+                wp_die('killing queue listener');
+            }
 
-		/**
-		 * Get job name.
-		 *
-		 * @return object
-		 */
-		public function get_job_name() {
-			return get_class( $this->payload );
-		}
+            return true;
+        }
 
-	}
+        /**
+         * Get job name.
+         *
+         * @return object
+         */
+        public function get_job_name()
+        {
+            return get_class($this->payload);
+        }
+    }
 }
